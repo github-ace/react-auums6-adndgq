18d5aae86fb1571610e9162eba77a7d19e7ea3a0
github-ace <2433195678@qq.com>
2020-03-11 17:54:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-auums6-adndgq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-auums6-adndgq)
---
+++ index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import "./index.css";
+import { Menu } from "antd";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined
+} from "@ant-design/icons";
+const { SubMenu } = Menu;
+
+class Sider extends React.Component {
+  handleClick = e => {
+    console.log("click ", e);
+  };
+
+  render() {
+    return (
+      <Menu
+        onClick={this.handleClick}
+        style={{ width: 256 }}
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+      >
+        <Menu.Item key="1">
+          <a>
+            <MailOutlined />
+            Option 1
+          </a>
+        </Menu.Item>
+        <SubMenu
+          key="sub2"
+          title={
+            <span>
+              <AppstoreOutlined />
+              <span>Navigation Two</span>
+            </span>
+          }
+        >
+          <Menu.Item key="5">Option 5</Menu.Item>
+          <Menu.Item key="6">Option 6</Menu.Item>
+          <SubMenu key="sub3" title="Submenu">
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item>
+          </SubMenu>
+        </SubMenu>
+        <SubMenu
+          key="sub4"
+          title={
+            <span>
+              <SettingOutlined />
+              <span>Navigation Three</span>
+            </span>
+          }
+        >
+          <Menu.Item key="9">Option 9</Menu.Item>
+          <Menu.Item key="10">Option 10</Menu.Item>
+          <Menu.Item key="11">Option 11</Menu.Item>
+          <Menu.Item key="12">Option 12</Menu.Item>
+        </SubMenu>
+      </Menu>
+    );
+  }
+}
+
+ReactDOM.render(<Sider />, document.getElementById("container"));
